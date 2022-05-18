f7dbc4956a5bc8bc504e2c7350df1133f7369ddb
dkozlov <->
2022-05-18 15:45:58 +0300

    Add auto removal flags for commands in dev.sh

--- dev.sh
+++ dev.sh
@@ -5,9 +5,9 @@ ACTION=$1
 if [[ $ACTION == "start" ]]
 then
      docker build --tag recipes:latest . \
-  && docker-compose --env-file env.dev run --entrypoint "./manage.py makemigrations" api \
-  && docker-compose --env-file env.dev run --entrypoint "./manage.py migrate" api \
+  && docker-compose --env-file env.dev run --rm --entrypoint "./manage.py makemigrations" api \
+  && docker-compose --env-file env.dev run --rm --entrypoint "./manage.py migrate" api \
   && docker-compose --env-file env.dev up --remove-orphans
 else
-     docker-compose --env-file env.dev down
+     docker-compose --env-file env.dev down --remove-orphans
 fi
