23715391fe9c87c19a57a9d2d66928079ee11a27
dkozlov <->
2022-07-01 17:22:07 +0300

    Add 'reset' command for dev.sh

--- dev.sh
+++ dev.sh
@@ -13,6 +13,12 @@ elif [[ $ACTION == "start" ]]; then
   && docker-compose --env-file env.dev up --remove-orphans
 elif [[ $ACTION == "stop" ]]; then
      docker-compose --env-file env.dev down --remove-orphans
+elif [[ $ACTION == "reset" ]]; then
+     docker-compose --env-file env.dev down --remove-orphans \
+  && docker volume rm \
+       "${PWD##*/}_postgres_data" \
+       "${PWD##*/}_redis_data" \
+       "${PWD##*/}_rabbitmq_data"
 else
   exit 2
 fi
